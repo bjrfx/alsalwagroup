6628c361e6c2b11822a3d495b7884df95f9ec4a9
bjrfx <phanikiran1234@gmail.com>
2023-04-06 01:22:35 +0530

    first commit
    Web template

---
+++ main.js
@@ -0,0 +1,12 @@
+// disable all buttons
+function disableButtons() {
+    document.getElementById('btn1').disabled = true;
+    document.getElementById('btn2').disabled = true;
+    document.getElementById('btn3').disabled = true;
+    document.getElementById('btn4').disabled = true;
+    document.getElementById('btn5').disabled = true;
+    document.getElementById('btn6').disabled = true;
+    document.getElementById('btn7').disabled = true;
+    document.getElementById('btn8').disabled = true;
+    document.getElementById('btn9').disabled = true;
+}
